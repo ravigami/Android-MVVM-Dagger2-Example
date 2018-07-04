11c3bc0bf1ac1bba2e51b658818dac803d908dc2
Uday Singh <uday144@gmail.com>
2018-07-04 15:58:11 +0530

    added Network Check

--- app/src/main/java/com/uday/androidsample/app/MyApplication.java
+++ app/src/main/java/com/uday/androidsample/app/MyApplication.java
@@ -3,6 +3,8 @@ package com.uday.androidsample.app;
 import android.app.Application;
 import com.uday.androidsample.diprovider.AppModule;
 import com.uday.androidsample.diprovider.ApiModule;
+import com.uday.androidsample.network.ConnectivityReceiver;
+
 /**
  * Created by Uday on 27/06/2018.
  */
@@ -10,11 +12,20 @@ import com.uday.androidsample.diprovider.ApiModule;
 public class MyApplication extends Application {
 
     private ApiComponent mApiComponent;
+    private static MyApplication mInstance;
 
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.ConnectivityReceiverListener listener) {
+        ConnectivityReceiver.connectivityReceiverListener = listener;
+    }
     @Override
     public void onCreate() {
         super.onCreate();
-
+        mInstance = this;
         mApiComponent = DaggerApiComponent.builder()
                 .appModule(new AppModule(this))
                 .apiModule(new ApiModule(Constant.BASE_URL))
--- app/src/main/java/com/uday/androidsample/fragments/FactsFragment.java
+++ app/src/main/java/com/uday/androidsample/fragments/FactsFragment.java
@@ -1,16 +1,21 @@
 package com.uday.androidsample.fragments;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.design.widget.Snackbar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.uday.androidsample.R;
 import com.uday.androidsample.adapter.CountryFactsAdapter;
 import com.uday.androidsample.app.Constant;
+import com.uday.androidsample.app.MyApplication;
 import com.uday.androidsample.model.Country;
 import com.uday.androidsample.model.Facts;
 import com.uday.androidsample.network.Api;
+import com.uday.androidsample.network.ConnectivityReceiver;
 import com.uday.androidsample.utils.MyDividerItemDecoration;
 import com.uday.androidsample.viewmodel.FactsViewModel;
 
@@ -30,7 +35,7 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class FactsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
+public class FactsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener,  ConnectivityReceiver.ConnectivityReceiverListener{
     RecyclerView recyclerView;
     CountryFactsAdapter adapter;
     SwipeRefreshLayout mSwipeRefreshLayout;
@@ -58,9 +63,7 @@ public class FactsFragment extends Fragment implements SwipeRefreshLayout.OnRefr
             @Override
             public void run() {
 
-                mSwipeRefreshLayout.setRefreshing(true);
-
-                // Fetching data from server
+               // Fetching data from server
                 loadDataToViewModel();
             }
         });
@@ -71,18 +74,22 @@ public class FactsFragment extends Fragment implements SwipeRefreshLayout.OnRefr
 
     private void loadDataToViewModel() {
         // Showing refresh animation before making http call
-        mSwipeRefreshLayout.setRefreshing(true);
-        FactsViewModel model = ViewModelProviders.of(this).get(FactsViewModel.class);
+        if(checkConnection()) {
+            mSwipeRefreshLayout.setRefreshing(true);
+            FactsViewModel model = ViewModelProviders.of(this).get(FactsViewModel.class);
 
-        model.getFacts().observe(this, new Observer<List<Facts>>() {
-            @Override
-            public void onChanged(@Nullable List<Facts> factsList) {
-                adapter = new CountryFactsAdapter(factsList, getActivity().getApplicationContext());
-                recyclerView.setAdapter(adapter);
-                // Stopping swipe refresh
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        });
+            model.getFacts().observe(this, new Observer<List<Facts>>() {
+                @Override
+                public void onChanged(@Nullable List<Facts> factsList) {
+                    adapter = new CountryFactsAdapter(factsList, getActivity().getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+                    // Stopping swipe refresh
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+            });
+        } else {
+            showSnack(false);
+        }
     }
 
     /**
@@ -122,5 +129,49 @@ public class FactsFragment extends Fragment implements SwipeRefreshLayout.OnRefr
                 Toast.makeText(getActivity().getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // Method to manually check connection status
+    private boolean checkConnection() {
+        return ConnectivityReceiver.isConnected();
+
+    }
+
+    // Showing the status in Snackbar
+    private void showSnack(boolean isConnected) {
+        String message;
+        int color;
+        if (isConnected) {
+            message = "Good! Connected to Internet";
+            color = Color.WHITE;
+        } else {
+            message = "Sorry! Not connected to internet";
+            color = Color.RED;
+        }
+
+        Snackbar snackbar = Snackbar
+                .make(getActivity().findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG);
+
+        View sbView = snackbar.getView();
+        TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setTextColor(color);
+        snackbar.show();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // register connection status listener
+        MyApplication.getInstance().setConnectivityListener(this);
+    }
+
+    /**
+     * Callback will be triggered when there is change in
+     * network connection
+     */
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        showSnack(isConnected);
     }
 }
